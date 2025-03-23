ef33f8274860a079e9c5fafc223acf6c0c0840e1
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-03-23 23:25:57 +0300

    fix(api): fixed base axios setup

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,7 +1,8 @@
 import axios, {type AxiosInstance} from "axios";
+import apiConf from "./api/apiConf.ts";
 
 const http: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: apiConf.endpoint,
     headers: {
         "Access-Control-Allow-Origin": "*",
         "Authorization": `Bearer ${localStorage.getItem('token')}`,
