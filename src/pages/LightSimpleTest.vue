e632928af7fb91ba2fe4dc652c3f42889b0bd22e
GetmanJule <465527@niuitmo.ru>
2025-03-31 22:42:30 +0300

    create page with simple light test

---
+++ src/pages/LightSimpleTest.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "LightSimpleTest"
+})
+
+</script>
+
+<template>
+    <div class="container">
+      <h1>Тест на скорость реакции</h1>
+      <p id="instruction">Нажмите на кнопку, когда она станет зелёной.</p>
+      <button id="reactionButton" disabled>Ждите...</button>
+      <p id="result"></p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
