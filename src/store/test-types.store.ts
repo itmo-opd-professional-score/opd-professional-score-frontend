b2632facd25954dc5abeb283283527c9983d0c2e
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-04-03 00:24:57 +0300

    feat(store): created test-types.store.ts

---
+++ src/store/test-types.store.ts
@@ -0,0 +1,33 @@
+import {defineStore} from "pinia";
+import type {TestTypeDataInputDto} from "../api/testType/dto/input/test-type-data-input.dto.ts";
+import {TestTypeResolver} from "../api/testType/testType.resolver.ts";
+import type {TestDataInputDto} from "../api/resolvers/test/dto/input/test-data-input.dto.ts";
+
+export const useTestTypesStore = defineStore("testTypes", {
+  state: () => ({
+    types: [] as TestTypeDataInputDto[]
+  }),
+  getters: {
+    getTestTypes: (state) => {
+      return state.types;
+    }
+  },
+  actions: {
+    async loadTestTypes() {
+      const testTypeResolver = new TestTypeResolver();
+      const types = await testTypeResolver.getAll();
+      if (types) {
+        this.types = types;
+      }
+    },
+    checkTestType(test: TestDataInputDto) {
+      let typeName;
+      this.types.map((type) => {
+        if (test.testTypeId == type.id) {
+          typeName = type.name;
+        }
+      });
+      return typeName;
+    }
+  },
+})
