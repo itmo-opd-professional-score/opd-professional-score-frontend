0bd0ce64a17668f5f164f6e85a3add82cc6a4a34
arthkinq <arthurslanov@gmail.com>
2025-04-06 21:09:30 +0300

    feat(component): created a test settings component

---
+++ src/components/SelectSettingsForm.vue
@@ -0,0 +1,231 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+import CommonButton from "./UI/CommonButton.vue";
+
+interface TestSettings {
+  testName: string
+  duration: number
+  showTimer: boolean
+  showMinuteResults: boolean
+  showTotalResults: boolean
+  showProgress: boolean
+  accelerationAmount: number
+  accelerationInterval: number
+  accelerationFrequency: number
+}
+
+export default defineComponent({
+  name: "SelectSettingsForm",
+  components: {CommonButton},
+  props: {
+    testName: {
+      required: true,
+      type: String,
+      default: "missing test name" as string,
+    }
+  },
+  data() {
+    return {
+      selectedInterval: 900,
+      showTimer: true,
+      showMinuteResults: true,
+      showTotalResults: true,
+      showProgress: true,
+      accelerationAmount: 10,
+      accelerationInterval: 60,
+      accelerationFrequency: 30
+    }
+  },
+  computed: {
+    formattedInterval() {
+      const minutes = Math.floor(this.selectedInterval / 60);
+      const seconds = this.selectedInterval % 60;
+      return `${minutes} мин ${seconds ? seconds : '00'} сек`;
+    }
+  }, methods: {
+    saveSettings() {
+      const settings: TestSettings = {
+        testName: this.testName,
+        duration: this.selectedInterval,
+        showTimer: this.showTimer,
+        showMinuteResults: this.showMinuteResults,
+        showTotalResults: this.showTotalResults,
+        showProgress: this.showProgress,
+        accelerationAmount: this.accelerationAmount,
+        accelerationInterval: this.accelerationInterval,
+        accelerationFrequency: this.accelerationFrequency
+      }
+      this.$emit('newSettings', settings)
+    }
+  },
+  emits: ['newSettings'],
+})
+</script>
+
+<template>
+  <div id="choose-settings">
+    <div class="choose-settings-form">
+      <div class="section">
+        <h2>Выберите настройки теста: {{ testName }}</h2></div>
+      <div class="settings-group">
+        <div class="time-interval-selector">
+          <label>Время прохождения теста: <strong>{{ formattedInterval }}</strong></label>
+          <input
+              type="range"
+              min="120"
+              max="2700"
+              step="30"
+              v-model="selectedInterval"
+              class="custom-slider"
+              @change=""
+          >
+          <div class="slider-labels">
+            <span>2 мин</span>
+            <span>45 мин</span>
+          </div>
+          <div class="setting-item">
+            <label>
+              <input type="checkbox" v-model="showTimer">
+              Отображать оставшееся время
+            </label>
+          </div>
+
+          <div class="setting-item">
+            <label>
+              <input type="checkbox" v-model="showMinuteResults">
+              Показывать результаты за минуту
+            </label>
+            <label>
+              <input type="checkbox" v-model="showTotalResults">
+              Показывать общий результат
+            </label>
+          </div>
+
+          <div class="setting-item">
+            <label>
+              <input type="checkbox" v-model="showProgress">
+              Отображать прогресс выполнения
+            </label>
+          </div>
+          <div class="acceleration-settings">
+            <h3>Настройки ускорения:</h3>
+            <div class="acceleration-input">
+              <label>Величина ускорения(%):
+                <input type="number" v-model.number="accelerationAmount" min="0" max="100">
+              </label>
+            </div>
+
+            <div class="acceleration-input">
+              <label>Интервал применения(от 1 до 1000):
+                <input type="number" v-model.number="accelerationInterval" min="1" max="1000">
+              </label>
+            </div>
+            <div class="acceleration-input">
+              <label>Как часто(от 1 до 1000):
+                <input type="number" v-model.number="accelerationFrequency" min="1" max="1000">
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section">
+        <CommonButton @click="saveSettings">
+          <template v-slot:placeholder>Сохранить настройки</template>
+        </CommonButton>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+#choose-settings {
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  z-index: 2;
+  backdrop-filter: blur(3px) saturate(50%);
+  transition: all 0.3s ease-out;
+}
+
+.choose-settings-form {
+  width: 40%;
+  margin: 14vh auto;
+  background: rgba(255, 255, 255, 1);
+  border-radius: 1.4rem;
+  color: black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow-clip-margin: 0.02rem;
+  overflow: clip;
+}
+
+.choose-settings-form h2 {
+  text-align: center;
+  color: white;
+}
+
+#choose-settings button {
+  margin: 0.3vh auto;
+}
+
+.section {
+  width: 100%;
+  background: rgb(16, 73, 231);
+  padding: 0.5rem;
+}
+
+.time-interval-selector {
+  width: 100%;
+  padding: 1rem;
+  color: black;
+}
+
+.time-interval-selector label {
+  display: block;
+  margin-bottom: 1rem;
+  font-size: 1.1rem;
+}
+
+.custom-slider {
+  width: 100%;
+  height: 0.4rem;
+}
+
+.slider-labels {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 0.1rem;
+  color: rgba(0, 0, 0, 0.7);
+}
+
+.settings-group {
+  padding: 1rem;
+  width: 100%;
+}
+
+.setting-item {
+  margin: 1rem 0;
+  padding: 0.5rem;
+  border-bottom: 1px solid #eee;
+}
+
+.acceleration-settings {
+  margin-top: 2rem;
+  padding: 1rem;
+  background: #f5f5f5;
+  border-radius: 8px;
+}
+
+.acceleration-input {
+  margin: 0.5rem 0;
+}
+
+.acceleration-input input {
+  margin-left: 0.5rem;
+  padding: 0.3rem;
+  width: 3vw;
+}
+
+</style>
