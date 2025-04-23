75872331839b668df15e5994e260b82cc8a468d5
Cass11037 <mihaelissebastam750@gmail.com>
2025-04-24 00:07:03 +0300

    feat(test): added methods for random colors, words and match

--- src/pages/StraupTest.vue
+++ src/pages/StraupTest.vue
@@ -3,6 +3,31 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'StraupTest',
+  data() {
+    return {
+      colors: ['red', 'green', 'yellow', 'blue', 'orange', 'pink', 'purple', 'black', 'white', 'gray'],
+      currentWord : '',
+      currentColor : '',
+      levelOfDifficulty: 0
+    }
+  },
+  methods : {
+    giveColor() {
+      this.currentWord = this.colors[Math.floor(Math.random() * 10)];
+    },
+    giveColorOfWord() {
+      if (this.currentWord !== '') {
+        if (Math.random() < this.givePropabilityOfWordAndColorMatch()) {
+          this.currentColor = this.currentWord
+        } else {
+          this.currentColor = this.colors[Math.floor(Math.random() * 10)];
+        }
+      }
+    },
+    givePropabilityOfWordAndColorMatch() {
+      return [0.7, 0.4,  0.1][this.levelOfDifficulty];
+    }
+  }
 });
 </script>
 
